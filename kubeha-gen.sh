cbed848c714f53d4ccdec35748a1b40ee8b9f1a2
henshitou <32926100+henshitou@users.noreply.github.com>
2019-03-06 12:54:18 +0000

    Update kubeha-gen.sh

--- kubeha-gen.sh
+++ kubeha-gen.sh
@@ -178,17 +178,17 @@ JOIN_CMD=`kubeadm token create --print-join-command`
 for index in 1 2; do
   host=${HOSTS[${index}]}
   ip=${IPS[${index}]}
-  ssh $HOSTS "mkdir -p /etc/kubernetes/pki/etcd; mkdir -p ~/.kube/"
-  scp /etc/kubernetes/pki/ca.crt $ip:/etc/kubernetes/pki/ca.crt
-  scp /etc/kubernetes/pki/ca.key $ip:/etc/kubernetes/pki/ca.key
-  scp /etc/kubernetes/pki/sa.key $ip:/etc/kubernetes/pki/sa.key
-  scp /etc/kubernetes/pki/sa.pub $ip:/etc/kubernetes/pki/sa.pub
-  scp /etc/kubernetes/pki/front-proxy-ca.crt $ip:/etc/kubernetes/pki/front-proxy-ca.crt
-  scp /etc/kubernetes/pki/front-proxy-ca.key $ip:/etc/kubernetes/pki/front-proxy-ca.key
-  scp /etc/kubernetes/pki/etcd/ca.crt $ip:/etc/kubernetes/pki/etcd/ca.crt
-  scp /etc/kubernetes/pki/etcd/ca.key $ip:/etc/kubernetes/pki/etcd/ca.key
-  scp /etc/kubernetes/admin.conf $ip:/etc/kubernetes/admin.conf
-  scp /etc/kubernetes/admin.conf $ip:~/.kube/config
+  ssh $host "mkdir -p /etc/kubernetes/pki/etcd; mkdir -p ~/.kube/"
+  scp /etc/kubernetes/pki/ca.crt $host:/etc/kubernetes/pki/ca.crt
+  scp /etc/kubernetes/pki/ca.key $host:/etc/kubernetes/pki/ca.key
+  scp /etc/kubernetes/pki/sa.key $host:/etc/kubernetes/pki/sa.key
+  scp /etc/kubernetes/pki/sa.pub $host:/etc/kubernetes/pki/sa.pub
+  scp /etc/kubernetes/pki/front-proxy-ca.crt $host:/etc/kubernetes/pki/front-proxy-ca.crt
+  scp /etc/kubernetes/pki/front-proxy-ca.key $host:/etc/kubernetes/pki/front-proxy-ca.key
+  scp /etc/kubernetes/pki/etcd/ca.crt $host:/etc/kubernetes/pki/etcd/ca.crt
+  scp /etc/kubernetes/pki/etcd/ca.key $host:/etc/kubernetes/pki/etcd/ca.key
+  scp /etc/kubernetes/admin.conf $host:/etc/kubernetes/admin.conf
+  scp /etc/kubernetes/admin.conf $host:~/.kube/config
 
   ssh ${HOSTS} "${JOIN_CMD} --experimental-control-plane"
 done
